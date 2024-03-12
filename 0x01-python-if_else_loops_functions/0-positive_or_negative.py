3c744e7488d6434f50d4bbc64e5225eb02f9f516
rallmac2 <root@tobijahdevice>
2024-03-12 10:19:22 +0100

    Positive anything is better than negative nothing

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+
+if number < 0:
+    print(f"{number} is negative")
+
+elif number > 0:
+    print(f"{number} is positive")
+
+elif number == 0:
+    print(f"{number} is zero")
+
+else:
+    print("input a valid number")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+I am now learning loops functions on python with alx, Come and Join me
