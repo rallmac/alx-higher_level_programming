21919bd1bb5e540f984ed99d4009563a56e9af69
rallmac2 <root@tobijahdevice>
2024-03-13 04:54:11 +0100

    zero to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for first_number in range(0, 10):
-    for second_number in range(first_number + 1, 10):
-        if first_number == 8 and second_number == 9:
-            print("{}{}".format(first_number, second_number))
-        else:
-            print("{}{}, ".format(first_number, second_number), end='')
+for mynumber in range(0, 100):
+    if mynumber == 99:
+        print("{}".format(mynumber))
+    else:
+        print("{:02d}, ".format(mynumber), end='')
