c9d0c50a2e7c142b70263b9ea278c059247e475a
rallmac2 <root@tobijahdevice>
2024-04-04 10:45:43 +0100

    Square with size3

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,32 +1,12 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
-
 
 class Square:
-    """Represent a square."""
-
-    def __init__(self, size=0):
-        """Initialize a new Rectangle.
+    """Class Square: defines a square"""
+    def __init__(self, size):
+        """
+        Initializes a square
 
         Args:
-            width (int): The width of the new square.
+            size (): size of the square
         """
-        self.size = size
-
-    @property
-    def size(self):
-        """Get/set the width of the square."""
-        return self.__size
-
-    @width.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    @property
-    def height(self):
-        """Get/set the height of the square."""
-        return self.__height
+        self.__size = size
