49dbab67f58e6072528e74aff9fc2949011fa2c4
rallmac2 <root@tobijahdevice>
2024-04-22 08:40:56 +0100

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from .base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def get_width(self):
+        return (self.__width)
+
+    def set_width(self, width):
+        self.__width = width
+
+    def get_height(self):
+        return (self.__height)
+
+    def set_height(self, height):
+        self.__height = height
+
+    def get_x(self):
+        return (self.__x)
+
+    def set_x(self, x):
+        self.__x = x
+
+    def get_y(self):
+        return (self.__y)
+
+    def set_y(self, y):
+        self.__y = y
