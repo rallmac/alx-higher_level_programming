6376ff50c23fd1f217aa475e1eb77390b37aed53
rallmac2 <root@tobijahdevice>
2024-04-23 12:04:06 +0100

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 class Base:
     __nb_objects = 0
 
@@ -8,4 +10,12 @@ class Base:
             self.id = id
         else:
             Base. __nb_objects += 1
-            self.id = Base. __nb_objects
+            self.id = Base.__nb_objects
+
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return (json.dumps(list_dictionaries))
