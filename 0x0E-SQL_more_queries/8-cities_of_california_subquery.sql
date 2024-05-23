38be3e498e2a30df0fffb10e386d995f371a866c
rallmac2 <root@tobijahdevice>
2024-05-23 13:57:48 +0100

    focusing on task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- This takes California database hbtn_0d_usa
 SELECT id, name FROM cities
 WHERE state_id = (
-	SELECT id FROM state
+	SELECT id FROM states
 	WHERE name = "California")
 ORDER BY id;
