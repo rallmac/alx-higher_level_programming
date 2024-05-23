15b4d051d061b062d511d73eb37df0210ae30989
rallmac2 <root@tobijahdevice>
2024-05-23 13:44:13 +0100

    another comment on task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
 -- This script creates a database and a table in a database
+-- This comment is taking another line
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
