fcc3a8d2b7609c28ea62dd69927ffc42ea650ba4
rallmac2 <root@tobijahdevice>
2024-05-23 14:02:10 +0100

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,5 @@
+-- This script imports database dump from hbtn_0d_tvshows
+-- to my own MySQL server 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
