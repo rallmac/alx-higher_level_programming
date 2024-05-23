8b304f6899368277b7aa85c46cc6222c6ed5de66
rallmac2 <root@tobijahdevice>
2024-05-23 18:25:47 +0100

    task 16 redo

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- This script list all the records in the table second_table
 -- of the database hbtn_0c_0
-SELECT score, COUNT(1) AS number FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT score, name
+FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
