1f2674de6d723288169e2a291a9c6067db76909f
rallmac2 <root@tobijahdevice>
2024-05-23 13:33:14 +0100

    comment adjustment badge A focus on task 9wq

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
--- This script lists cities found in a datase
--- The cities found in California
+-- This script lists cities found in a datase the cities found in California
 SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id FROM state
