d8ce39ffc3a4a430196135ae6f1e995588267efb
rallmac2 <root@tobijahdevice>
2024-05-23 13:01:03 +0100

    task 0 comment again

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
---This script lists all privileges of the sql server 
-
+-- This script lists all privileges of the sql server 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
