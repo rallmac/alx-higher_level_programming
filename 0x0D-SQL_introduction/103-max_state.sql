b3bb4bf8f5f55a87e96598a34039441c248808e3
rallmac2 <root@tobijahdevice>
2024-05-24 07:57:31 +0100

    task 103 redo

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -2,9 +2,7 @@
 -- ordered by the state name, of an imported dump table
 -- called temperatures.sql
 
-mysql -u -p hbtn_0c_0 < temperatures.sql
-
-SELECT state, MAX(temperature) AS max_temperature
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
