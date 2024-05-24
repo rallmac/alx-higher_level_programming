590d274666825a6e5d00fd5cba5a1806ce037d7f
rallmac2 <root@tobijahdevice>
2024-05-24 08:36:17 +0100

    task 100 redo again and again

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,6 @@
 -- This script works like the previous script in
 -- task 16
-SELECT DISTINCT `name` 
+SELECT DISTINCT `title` 
 	FROM `tv_genres` AS g
 		INNER JOIN `tv_show_genres` AS s
 		ON g.`id` = s.`genre_id`
