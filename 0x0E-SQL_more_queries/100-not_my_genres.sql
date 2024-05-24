15041f5723dcdc89595dd06046c2f98a3f2aa4a4
rallmac2 <root@tobijahdevice>
2024-05-24 08:18:12 +0100

    task 100 redo

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,19 @@
 -- This script works like the previous script in
 -- task 16
-SELECT name FROM tv_genres
-WHERE tv_genres.id NOT IN (
-	SELECT tv_genres.id FROM tv_genres
-	JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
-	JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
-	WHERE tv_shows.title = "DEXTER" )
-ORDER BY tv_genres.name;
+SELECT DISTINCT `name` 
+FROM `tv_genres` AS g
+	INNER JOIN `tv_shows_genres` AS s
+	ON g.`id` = s.`genre_id`
+
+	INNER JOIN `tv_shows` AS t
+	ON s.`show_id` = t.`id`
+	WHERE g.`name` NOT IN
+		(SELECT `name`
+			FROM `tv_genres` AS g
+				INNER JOIN `tv_show_genres` AS s
+				ON g.`id` = s.`genre_id`
+
+				INNER JOIN `tv_shows` AS t
+				ON s.`show_id` = t.`id`
+				WHERE t.`title` = "Dexter")
+ORDER BY g.`name`;
