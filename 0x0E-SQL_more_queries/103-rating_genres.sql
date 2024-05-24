ff241ac1baad60a6b7517a0bc6b9e613978269ba
rallmac2 <root@tobijahdevice>
2024-05-24 08:41:41 +0100

    task 103 redo

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,19 @@
--- This script works like the previous script
--- it imports a database from hbtn_0d_tvhsows
--- into my own MySQL
-SELECT CONCAT(tv_genres.name, ' - ', SUM(tv_show_genres.rating)) 
-AS genre_rating FROM tv_genres JOIN tv_show_genres ON
-(link unavailable) = tv_show_genres.genre_id GROUP BY
-tv_genres.name ORDER BY SUM(tv_show_genres.rating) DESC;
+-- This script works like the previous script in
+-- task 16
+SELECT DISTINCT `name` 
+	FROM `tv_genres` AS g
+		INNER JOIN `tv_show_genres` AS s
+		ON g.`id` = s.`genre_id`
+
+		INNER JOIN `tv_shows` AS t
+		ON s.`show_id` = t.`id`
+		WHERE g.`name` NOT IN
+			(SELECT `name`
+				FROM `tv_genres` AS g
+					INNER JOIN `tv_show_genres` AS s
+					ON g.`id` = s.`genre_id`
+
+					INNER JOIN `tv_shows` AS t
+					ON s.`show_id` = t.`id`
+					WHERE t.`title` = "Dexter")
+ORDER BY g.`name`;
