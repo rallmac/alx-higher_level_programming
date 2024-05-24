bbb87123f9361c2cb87d46bc62b6270987f43268
rallmac2 <root@tobijahdevice>
2024-05-24 04:33:00 +0100

    task 18 redo

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,8 @@
 -- This script imports a table into my database called
 -- hbtn_0c_0
 mysql -u -p hbtn_0c_0 < temperatures.sql
+
+SELECT city, AVG(temperature) AS average_temperature
+FROM temperatures
+GROUP BY city
+ORDER BY average_temperature DESC;
