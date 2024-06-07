8ac496dfe0e6fdaf3049ed4185d5cffa43a20c0c
rallmac2 <root@tobijahdevice>
2024-06-07 23:05:08 +0100

    tast 2 redo again

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,7 @@ This Class inherits from list
 
 
 class MyList(list):
+    """ This is the list class to inherit """
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
