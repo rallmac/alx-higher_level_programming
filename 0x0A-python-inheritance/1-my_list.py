4986cb51380c8565643c9034183aca32fd988646
rallmac2 <root@tobijahdevice>
2024-06-08 12:41:04 +0100

    task 1 redo - again

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,7 @@ This Class inherits from list
 class MyList(list):
     """ This is the list class to inherit """
     def print_sorted(self):
-        """ This attribite inherits from the class """
+        pass
+        """ This attribite inherits from class """
         sorted_list = sorted(self)
         print(sorted_list)
