14134053c1fe6b47e6ea402f6211ab705877ec6b
rallmac2 <root@tobijahdevice>
2024-06-11 14:00:09 +0100

    task 7 redo

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
+"""
+This module has a class BaseGometry with two public instance
+attributes
+"""
+
 
 class BaseGeometry:
+    """
+    The BaseGeometry class
+    """
     def area(self):
+        """
+        The area public instance attribute that raise an
+        exception if value is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        The public instance attribute that validates the input
+        to ensure that the input is an integer and is greater than
+        zero
+        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
