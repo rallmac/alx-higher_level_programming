fbcecdedbfe271bff832f952ec3ed547e0832ef8
rallmac2 <root@tobijahdevice>
2024-06-11 14:34:02 +0100

    task 8 redo

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""
+This module contains a class Rectangle with an initialitation
+attribute. The rectangle class inherits the BaseGeometry class
+"""
+
 
 class Rectangle(BaseGeometry):
+    """
+    The rectangle class that inherits the BaseGeometry
+    """
     def __init__(self, width, height):
+        """
+        The init method. In this method, the width is a private
+        and the height is only positive integer
+        """
         self.__width = 0
         self.__height = 0
         self.integer_validator("width", width)
