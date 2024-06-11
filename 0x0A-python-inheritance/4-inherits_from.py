80cc1b254aa817b543c95f66da102dbfe02de0aa
rallmac2 <root@tobijahdevice>
2024-06-11 11:10:07 +0100

    task 4 redo

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+This module returns true if an object is an instance
+if a class inherited from the specified class
+"""
+
 
 def inherits_from(obj, a_class):
+    """
+    The function of that inherits from a class
+    """
     obj_class = type(obj)
 
     if obj_class == a_class:
@@ -9,5 +17,5 @@ def inherits_from(obj, a_class):
     for base_class in obj_class.__bases__:
         if inherits_from(base_class, a_class):
             return True
-    
+
     return False
