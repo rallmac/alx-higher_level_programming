c85aababf81136c62700e0d2825584b5e8c5dd75
rallmac2 <root@tobijahdevice>
2024-06-11 14:59:34 +0100

    task 10 redo

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-
+""" This module contains the rectangle class """
 Rectangle = __import__('9-rectangle').Rectangle
 
+
+
 class Square(Rectangle):
+    """ A subclass that represents a rectangle """
     def __init__(self, size):
+        """ The init constructor """
         self.__size = 0
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def __str__(self):
+        """ This method returns area of the square """
         return "[Square] {}/{}".format(self.__size, self.__size)
