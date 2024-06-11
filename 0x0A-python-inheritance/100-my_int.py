d31b3e015d0603d49c4c3e11fac9a48bbdcf8f83
rallmac2 <root@tobijahdevice>
2024-06-11 15:50:47 +0100

    task 100 redo

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+""" This module contains a class MyInt """
+
 
 class MyInt(int):
+    """ Another version of an integer """
     def __eq__(self, other):
+        """ changing the value of != to == """
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """ changing the value of == to != """
         return super().__eq__(other)
