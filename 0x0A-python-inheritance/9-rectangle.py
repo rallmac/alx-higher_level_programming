1d8805c707e9c2c57c912cd8aad5645cde4c5986
rallmac2 <root@tobijahdevice>
2024-06-11 14:48:36 +0100

    task 9 redo

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
+"""
+This module inherits the base geometry class
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+    """ The class that inherits BaseGeometry """
     def __init__(self, width, height):
         self.__width = 0
         self.__height = 0
@@ -12,8 +16,9 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
+        """ The public instance attribute of area """
         return self.__width * self.__height
 
     def __str__(self):
+        """ Returns the width and height description"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
