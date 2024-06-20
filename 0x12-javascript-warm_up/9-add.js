65c1eb8a01986b2d180f2e9b8a61a1384f29d84c
rallmac2 <root@tobijahdevice>
2024-06-20 18:43:54 +0100

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const a = Number(args[0]);
+const b = Number(args[1]);
+
+function add(a, b) {
+  return a + b;
+}
+
+if (!isNaN(a) && !isNaN(b)) {
+  const sum = add(a, b);
+  console.log(sum);
+} else {
+  console.log('NaN');
+}
