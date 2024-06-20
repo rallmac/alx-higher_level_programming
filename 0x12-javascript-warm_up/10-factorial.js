b034c77dfd59d338036b53acd064e230dbe71b3c
rallmac2 <root@tobijahdevice>
2024-06-20 19:30:35 +0100

    task 10 redo

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,17 +5,17 @@ const args = process.argv.slice(2);
 const firstArg = args[0];
 const n = parseInt(firstArg, 10);
 
-function factorial(n) {
+function factorial (n) {
   if (n === 0) {
-    return n = 1;
-}
+    return 1;
+  }
 
-return n * factorial(n - 1);
+  return n * factorial(n - 1);
 }
 
 if (!isNaN(n)) {
-  const result  = factorial(n);
+  const result = factorial(n);
   console.log(`${result}`);
 } else {
-  console.log(`1`);
+  console.log('1');
 }
