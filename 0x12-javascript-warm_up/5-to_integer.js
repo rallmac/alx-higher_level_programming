d2d69abeec91ded21920bfaa71805ad09a0a2854
rallmac2 <root@tobijahdevice>
2024-06-20 16:28:27 +0100

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const firstArg = args[0];
+const num = parseInt(firstArg, 10);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log(`Not a number`);
+}
