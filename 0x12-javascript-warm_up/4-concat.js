d952dc080677ce365d7a2bd6ad759e1985d81471
rallmac2 <root@tobijahdevice>
2024-06-20 15:58:06 +0100

    task 4 redo

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 
-const firstArg = args[0] !== undefined ? args[0] : 'udefined';
+const firstArg = args[0] !== undefined ? args[0] : 'undefined';
 const secondArg = args[1] !== undefined ? args[1] : 'undefined';
 
 console.log(`${firstArg} is ${secondArg}`);
