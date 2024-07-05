3d1a21e39fed893f6574abe5082d539ef43409db
rallmac2 <root@tobijahdevice>
2024-07-05 03:33:06 +0100

    task 0 redo

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-# This script takes in a url as input and returns the
-# size of the body
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-URL=$1
-
-response_size=$(curl -s -w '%{size_download}' -o /dev/null "$URL")
-
-echo "${response_size}"
+# This script takes in a url as input and returns the size of the body
+curl -s "$1" | wc -c
