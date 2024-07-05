fdae803504c3ed0e78228db64b910dca2bbe4a3a
rallmac2 <root@tobijahdevice>
2024-07-05 19:53:25 +0100

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script takes in a url and an email the passed url"""
+
+import urllib.request
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    headers = response.getheaders()
+
+    x_request_id = dict(headers).get('X-Request-Id')
