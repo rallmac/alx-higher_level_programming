c84dbd6617a1afa81ee8e9a6e878df3571f837e7
rallmac2 <root@tobijahdevice>
2024-08-08 03:38:16 +0100

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+/* global $ */
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    const movies = data.results;
+    const $list = $('#list_movies');
+
+    movies.forEach(function (movie) {
+      $list.append('<li>' + movie.title + '</li>');
+    });
+  });
+});
